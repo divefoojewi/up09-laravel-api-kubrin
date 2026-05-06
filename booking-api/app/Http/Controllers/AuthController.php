fd4381b8f79cf9ac401027025a8eaad5f6adcbb7
Bogdan <diveffff@gmail.com>
2026-05-06 04:34:13 +0000

    laravel

---
+++ booking-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Hash;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name'=> $data['name'],
+            'email'=> $data['email'],
+            'password'=> Hash::make($data['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'access_token'=> $token,
+            'token_type' => 'Bearer'
+            ], 201);
+    }
+}
