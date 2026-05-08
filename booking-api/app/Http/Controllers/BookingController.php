95fd49acd4dfe766564985e6768379bbade837f6
Bogdan <diveffff@gmail.com>
2026-05-08 05:33:41 +0000

    lara

--- booking-api/app/Http/Controllers/BookingController.php
+++ booking-api/app/Http/Controllers/BookingController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BookingController extends Controller
@@ -19,9 +21,18 @@ class BookingController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(Request $request)
     {
-        //
+        $data = $request->validate([
+            'room_name' => 'required|string|max:100',
+            'starts_at' => 'required|date|after:now',
+            'ends_at' => 'required|date|after:starts_at',
+            'note' => 'nullable|string|max:500',
+        ]);
+
+        $booking = $request->user()->bookings()->create($data);
+
+        return response()->json($booking, 201);
     }
 
     /**
@@ -59,8 +70,9 @@ class BookingController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Request $request, Booking $booking): JsonResponse
     {
-        //
+        $booking->delete();
+        return response()->json(["message" => "Встреча отменена"], 200);
     }
 }
--- booking-api/routes/api.php
+++ booking-api/routes/api.php
@@ -12,9 +12,10 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
-Route::post('/logout',[AuthController::class,'logout']);
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/bookings',[BookingController::class,'index']);
+    Route::post('/bookings',[BookingController::class,'create']);
+    Route::delete('/bookings/{booking}',[BookingController::class,'destroy']);
 });
